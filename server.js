4646da98a3a708d2193203f16afca716f90d02e1
Catalizcs <rfechinonguyen@gmail.com>
2020-05-28 15:22:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const osmosis = require('osmosis');
 
-const searchByImage = (url, callBack) => {
-    osmosis
-    .get('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(url))
-    .find('div:nth-child(4) > div > div > div > div > div.r > a')
-    .set({
-      url: '@href',
-      title: 'h3'
-    })
-    .data(function (listing) {
-      callBack(listing)
-    })
+const searchByImage = async (url) => {
+	let results = [];
+	try {
+		await osmosis
+			.get('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(url))
+			.set([
+				osmosis
+					.find('div:nth-child(4) > div > div > div > div > div.r > a')
+					.set({
+						header: 'h3',
+						link: '@href',
+					})
+					.data((site) => {
+						site.header = site.header.replace(/[^\w\s]/gi, '');
+					})
+			])
+			.data((sites) => {
+				results = sites;
+			});
+		return results;
+	} 
+	catch (error) {
+		throw new Error(400);
+	}
 }
 
 module.exports = searchByImage;
