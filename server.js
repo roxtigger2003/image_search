f7177ad7723afa20a2f93335cdfa5db3fe4cf12a
Catalizcs <rfechinonguyen@gmail.com>
2020-05-28 15:34:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,23 @@ const osmosis = require('osmosis');
 
 const searchByImage = async (url) => {
 	let results = [];
-	try {
-		await osmosis
-			.get('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(url))
-			.set([
-				osmosis
-					.find('div:nth-child(4) > div > div > div > div > div.r > a')
-					.set({
-						header: 'h3',
-						link: '@href',
-					})
-					.data((site) => {
-						site.header = site.header.replace(/[^\w\s]/gi, '');
-					})
-			])
-			.data((sites) => {
-				results = sites;
-			});
-		return results;
-	} 
-	catch (error) {
-		throw new Error(400);
-	}
+	await osmosis
+		.get('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(url))
+		.set([
+			osmosis
+				.find('div:nth-child(4) > div > div > div > div > div.r > a')
+				.set({
+					header: 'h3',
+					link: '@href',
+				})
+				.data((site) => {
+					site.header = site.header.replace(/[^\w\s]/gi, '');
+				})
+		])
+		.data((sites) => {
+			results = sites;
+		}).catch(error => console.log(error));
+	return results;
 }
 
 module.exports = searchByImage;
